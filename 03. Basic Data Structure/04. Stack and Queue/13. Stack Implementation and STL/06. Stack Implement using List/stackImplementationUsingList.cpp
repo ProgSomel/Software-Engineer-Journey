bb02aff3fb6c59b27186a577cab1cc8713674e62
Somel <somelahmed88@gmail.com>
2023-06-26 13:26:40 +0600

    Stack and Queue Added

---
+++ 03. Basic Data Structure/04. Stack and Queue/13. Stack Implementation and STL/06. Stack Implement using List/stackImplementationUsingList.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class myStack {
+    public:
+      list<int> myList;
+
+      void push(int val) {
+        myList.push_back(val);
+      }
+      void pop() {
+        myList.pop_back();
+      }
+      int top() {
+        return myList.back();
+      }
+      int size() {
+        return myList.size();
+      }
+      bool empty() {
+        if(myList.size() == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }    
+        
+      }
+};
+
+int main()
+{
+    myStack st;
+    int n;
+    cin>>n;
+    while(n--) {
+       int x;
+       cin>>x;
+       st.push(x);
+    }
+    while(!st.empty()) {
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+    return 0;
+}
